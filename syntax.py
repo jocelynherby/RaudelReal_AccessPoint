287af503fd7a95675fc4c9474d2eff1b9f64bda3
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-23 14:33:44 -0600

    Create syntax.py

---
+++ syntax.py
@@ -0,0 +1,8 @@
+if 5 > 2:
+  print('five is greater than two')
+  if 5 > 2:
+    print('five is greater than two')
+    x = 5 
+    y = Hello World
+    #this is a comment.
+    print('Hello, World')
