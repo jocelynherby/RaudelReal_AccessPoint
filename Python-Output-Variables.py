3706682b8b3286ddc426469997f556576c69f549
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-23 16:07:42 -0600

    Create Python-Output-Variables.py

---
+++ Python-Output-Variables.py
@@ -0,0 +1,26 @@
+x = 'python is awesome'
+print(x)
+
+x = 'python'
+y = 'is'
+z = 'awesome'
+print(x, y, z)
+x = 'python '
+y = 'is '
+z = 'awesome'
+print(x + y + z)
+
+#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+x = 5 
+y = 10 
+print(5 + 10)
+
+x = 5 
+y = 'john'
+print(x + y)
+
+x = 5 
+y = 'john'
+print(x, y)
+#commas will support different data types
