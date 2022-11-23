2f510b99c15d759ed84bf424b412f574b61970f4
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-23 14:46:31 -0600

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,12 @@
+#this is a comment
+print('Hello, World') # This is a comment
+#print('Hello, World')
+print('Cheers, mate!')
+#this is a comment 
+#written in 
+#more than just one line
+print('Hello, World')
+"""this is a comment
+written in
+more than just one line"""
+print('hello, world')
