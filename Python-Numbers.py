2596ba49eba3b27190a6e231787eec84a9bfe6d8
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-24 09:41:02 -0600

    Create Python-Numbers.py

---
+++ Python-Numbers.py
@@ -0,0 +1,23 @@
+x = 1  #int
+y = 2.8  #float
+z = 1j  #complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+#integer is a whole number with infinate langth and does not have a decimal
+x = 1
+y = 364512476553564342157434
+z = -38687386353
+#a float number contains a one or more decimals, negative or positive
+x = 1.10
+y = 1.0
+z = -35.59
+#floats can also be a scientific number with an E to indicate the power of 10
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
