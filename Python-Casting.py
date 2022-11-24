7480be35a672567662e4c5635416021cc7b9ad88
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-24 17:53:47 -0600

    Create Python-Casting.py

---
+++ Python-Casting.py
@@ -0,0 +1,19 @@
+#casting an intrger variable
+x = int(1)  #x will be 1
+y= int(2.8)  #y will be 2
+z = int('3')  #z willl be 3
+#casting a float variabale
+x = float(1)  #x will be 1.0
+y = float(2.8)  #y will be 2.8
+z = float('3') #z will be 3.0
+w = float('4.2') #w will bw 4.2
+#casting a string variable
+x = str("s1")  #x will be 's1'
+y = str(2)  #y will be '2'
+y = str(3.0)  #z will be '3.0'
+
+#int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+
+#float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+
+#str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
