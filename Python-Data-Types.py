6f91ef2f05e8414685308dbdeaeee7f88f2ac56e
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-24 09:20:57 -0600

    Create Python-Data-Types.py

---
+++ Python-Data-Types.py
@@ -0,0 +1,33 @@
+x = 5
+print(type(x)) #finding the data type
+#setting the data type
+x = 'hello, wolrd!'
+x = 20
+x = 20.5
+x = 1j
+x = ['apple', 'banana', 'charry']
+x = ('apple', 'banana', 'charry')
+x = range(6)
+x = {'name' : 'john', 'age' : 36}
+x = {'apple', 'banana', 'charry'}
+x = frozenset({'apple', 'banna', 'cherry'})
+x = True
+x = b'hello'
+x = bytearray(5)
+x = memoryview(bytes(5))
+x = None
+#Setting the Specific Data Type
+x = str("Hello World")	#str	
+x = int(20)	#int	
+x = float(20.5)	#float	
+x = complex(1j)	#complex	
+x = list(("apple", "banana", "cherry"))	#list	
+x = tuple(("apple", "banana", "cherry"))	#tuple	
+x = range(6)	#range	
+x = dict(name="John", age=36)	#dict	
+x = set(("apple", "banana", "cherry"))	#set	
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+x = bool(5)	#bool	
+x = bytes(5)	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview
