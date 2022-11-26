be937cc59067e767af53c4472d873e450a1f7dc6
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-26 10:30:18 -0600

    Create Python-Modify-Strings.py

---
+++ Python-Modify-Strings.py
@@ -0,0 +1,19 @@
+a = 'hello, world!'
+print(a.upper())
+#make all the srings upper case by using "upper()"
+
+a = 'hello, world!'
+print(a.lower())
+##make all the srings lower case by using "lower()"
+
+a = ' hello, world! '
+print(a.strip()) # returns 'hello, world!'
+#removes the whitespace before and/or after the actual text by using "strip()"
+
+a = 'hello, world!'
+print(a.replace('h', 'j'))
+#replaces one string with another by using "replace()"
+
+a = 'hello, world!'
+print(a.split(',')) #returns ['hello', 'world!']
+#splits the string into substrings if it finds instances of the separator by using "split()"
