a42004ce5232bd9fc3c462ac33b658b2296e4445
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-26 19:58:30 -0600

    Create Python-Escape-Characters.py

---
+++ Python-Escape-Characters.py
@@ -0,0 +1,16 @@
+txt = 'we are the so-called 'Vikings' from the north' 
+#no can do, you'll get an error
+
+txt = 'we are the so-called /'Vikings' from the north
+#to fix it use a "/"
+
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+#other characters 
