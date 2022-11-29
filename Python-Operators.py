875bcd14c40a03ee62e9b0e471c8111953cfe4ef
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-28 21:55:41 -0600

    Create Python-Operators.py

---
+++ Python-Operators.py
@@ -0,0 +1,66 @@
+print(5 + 10)
+
+y = 23
+x = 14
+
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+x = 5 
+print(x)
+x += 3
+print(x)
+x -= 3
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x %= 3
+print(x)
+x //= 3
+print(x)
+x **= 3
+print(x)
+#x &= 3
+print(x)
+#x |= 3
+print(x)
+#x ^= 3
+print(x)
+#x >>= 3 
+print(x)
+#x <<= 3
+print(x)
+
+x = 5
+y = 3
+x == y
+x != y
+x > y
+x < y
+x >= y 
+x <= y 
+
+x = 5
+x < 5 and x < 10
+x < 5 or x < 4
+not(x < 5 and x < 10)
+
+#in 
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
+#not in
+x = ["apple", "banana"]
+print("pineapple" not in x)
