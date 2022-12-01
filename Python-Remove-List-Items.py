7f6c24a6a20dda435a099abe7105701e6f5dfddd
RaudelReal701 <118926920+RaudelReal701@users.noreply.github.com>
2022-11-30 21:44:08 -0600

    Create Python-Remove-List-Items.py

---
+++ Python-Remove-List-Items.py
@@ -0,0 +1,27 @@
+thisList = ['apple', 'banana', 'cherry']
+thisList.remove('banana')
+print(thisList)
+#to remove an item
+
+thisList = ['apple', 'banana', 'cherry']
+thisList.pop(1)
+print(thisList)
+#to remove an index
+
+thisList = ['apple', 'banana', 'cherry']
+thisList.pop()
+print(thisList)
+#it'll remove the last one, if there is no index
+
+thisList = ['apple', 'banana', 'cherry']
+del thisList[0]
+print(thisList)
+
+thisList = ['apple', 'banana', 'cherry']
+del thisList 
+#print(thisList)
+#removes the list as a whole
+thisList = ['apple', 'banana', 'cherry']
+thisList.clear()
+print(thislist
+     #this list as no content, but does exist
